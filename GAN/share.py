5ed9e1fa81cd315a1a5b35f7c598cbeced56bfe7
xi-studio <110littletree@sina.com>
2019-06-11 11:41:42 +0800

    share AE

---
+++ GAN/share.py
@@ -0,0 +1,156 @@
+from __future__ import print_function
+import argparse
+import torch
+import torch.utils.data
+from torch import nn, optim
+from torch.nn import functional as F
+from torchvision import datasets, transforms
+from torchvision.utils import save_image
+
+
+parser = argparse.ArgumentParser(description='VAE MNIST Example')
+parser.add_argument('--batch-size', type=int, default=128, metavar='N',
+                    help='input batch size for training (default: 128)')
+parser.add_argument('--epochs', type=int, default=10, metavar='N',
+                    help='number of epochs to train (default: 10)')
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='enables CUDA training')
+parser.add_argument('--seed', type=int, default=1, metavar='S',
+                    help='random seed (default: 1)')
+parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                    help='how many batches to wait before logging training status')
+args = parser.parse_args()
+args.cuda = not args.no_cuda and torch.cuda.is_available()
+
+torch.manual_seed(args.seed)
+
+device = torch.device("cuda" if args.cuda else "cpu")
+
+kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
+train_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('./data', train=True, download=True,
+                   transform=transforms.ToTensor()),
+    batch_size=args.batch_size, shuffle=True, **kwargs)
+test_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('./data', train=False, transform=transforms.ToTensor()),
+    batch_size=args.batch_size, shuffle=False, **kwargs)
+
+norm = torch.nn.functional.normalize
+soft = nn.Softmax(dim=1)
+
+class VAE(nn.Module):
+    def __init__(self):
+        super(VAE, self).__init__()
+
+        self.fc1 = nn.Linear(784, 400)
+        self.fc21 = nn.Linear(400, 3)
+        self.fc22 = nn.Linear(400, 3)
+        self.fc3 = nn.Linear(13, 400)
+        self.fc4 = nn.Linear(400, 784)
+
+        self.share = nn.Sequential(
+            nn.Linear(784, 400),
+            nn.ReLU(),
+        )
+
+    def encode(self, x, B):
+        x = torch.sigmoid(self.fc1(x))
+        x = x * B 
+
+        n = F.relu(self.fc21(x))
+        s = torch.sigmoid(self.fc22(x))
+        x = n * s
+        z = norm(x, dim=1)
+       
+        return z
+
+    def decode(self, z, B):
+        x = torch.sigmoid(self.fc3(z))
+        x = x * B
+
+        return torch.sigmoid(self.fc4(x))
+
+    def forward(self, x, c):
+        s = torch.ones_like(x)
+        B = self.share(s)
+
+        z = self.encode(x.view(-1, 784), B)
+        x = torch.cat((z, c),dim=1)
+        x = self.decode(x, B)
+
+        return x, z
+
+
+model = VAE().to(device)
+optimizer = optim.Adam(model.parameters(), lr=1e-4)
+
+
+mse = nn.MSELoss(reduction='sum')
+def loss_function(recon_x, x):
+    BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784), reduction='sum')
+
+    return BCE
+
+
+def train(epoch):
+    model.train()
+    train_loss = 0
+    for batch_idx, (data, c) in enumerate(train_loader):
+
+        c = c.unsqueeze(1)
+        c_onehot = torch.zeros(data.shape[0], 10)
+        c_onehot.scatter_(1, c, 1)
+        c_onehot = c_onehot.to(device)
+
+        data = data.to(device)
+        optimizer.zero_grad()
+        rx, z = model(data, c_onehot)
+        loss = loss_function(rx, data)
+        loss.backward()
+        train_loss += loss.item()
+        optimizer.step()
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader),
+                loss.item() / len(data)))
+
+    if epoch % 50 == 0:
+        torch.save(model.state_dict(),"checkpoints/mnist/share_%03d.pt" % epoch)
+    
+
+    print('====> Epoch: {} Average loss: {:.4f}'.format(
+          epoch, train_loss / len(train_loader.dataset)))
+
+
+def test(epoch):
+    model.eval()
+    test_loss = 0
+    with torch.no_grad():
+        for i, (data, c) in enumerate(test_loader):
+            c = c.unsqueeze(1)
+            c_onehot = torch.zeros(data.shape[0], 10)
+            c_onehot.scatter_(1, c, 1)
+            c_onehot = c_onehot.to(device)
+            data = data.to(device)
+            rx, z = model(data, c_onehot)
+            loss = loss_function(rx, data)
+            test_loss += loss.item()
+            if i == 0:
+                c1 = torch.zeros_like(c_onehot)
+                c1[:,6] = 1
+                x = torch.cat((z, c1),dim=1)
+                x = model.decode(x)
+                n = min(data.size(0), 16)
+                img = torch.cat((data[:64].view(-1,784), rx[:64], x[:64]),dim=0)
+                img = img.view(64*3, 1, 28, 28)
+                save_image(img.cpu(),
+                         'images/share_sample_' + str(epoch) + '.png', nrow=64)
+
+    test_loss /= len(test_loader.dataset)
+    print('====> Test set loss: {:.4f}'.format(test_loss))
+
+if __name__ == "__main__":
+    for epoch in range(1, args.epochs + 1):
+        train(epoch)
+        test(epoch)
